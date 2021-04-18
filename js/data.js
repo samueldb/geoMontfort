925bd071df955b8dde1ce55b4b8a5459868d6755
samueldb <samuel.deschampsberger@gmail.com>
2021-04-18 11:12:31 +0200

    feat(data): add hydro source and layers

--- js/data.js
+++ js/data.js
@@ -28,9 +28,6 @@ function getCadastreLayerFromStMarcel(layerName, codeCommune){
     })
 }
 
-function sortByLabel(features) {
-    return _.sortBy(features, function (f) { return f.properties.label })
-}
 
 function getSections(codeCommune){
     return getCadastreLayerFromStMarcel('sections', codeCommune).then(function(featureCollections){
@@ -56,11 +53,74 @@ function getParcelles(codeCommune, idSection){
         return {
             type: 'FeatureCollection',
             features: _.chain(featureCollection.features)
-                .filter(function(f){
-                    return f.id.startsWith(idSection)
-                })
+                // .filter(function(f){
+                //     return f.id.startsWith(idSection)
+                // })
                 .sortBy('id')
                 .value()
         }
     })
+}
+
+function getHydrographie(center){
+    return getRemoteGeoJson('https://public.opendatasoft.com/api/records/1.0/search/?' +
+                                    'dataset=hydrographie-cours-deau&q=' +
+                                    '&clusterprecision=17' +
+                                    '&rows=200' +
+                                    '&facet=artif&facet=fictif&facet=franchisst&facet=nom&facet=pos_sol&facet=regime&facet=commune&facet=epci_name&facet=dep_name&facet=reg_name' +
+                                    // '&geofilter.distance='+center+',10000'+
+        '&geofilter.polygon=(45.477,6.551),(45.477,6.609),(45.505, 6.609),(45.505,6.551),(45.477,6.551)'
+    )
+        // [
+        // 6.550598144531249,
+        //     45.47722544469191
+        // ],
+        // [
+        //     6.609134674072266,
+        //     45.47722544469191
+        // ],
+        // [
+        //     6.609134674072266,
+        //     45.50219620881016
+        // ],
+        // [
+        //     6.550598144531249,
+        //     45.50219620881016
+        // ],
+        // [
+        //     6.550598144531249,
+        //     45.47722544469191
+        // ]
+        .then(function(returnFeatures){
+            var acc = returnFeatures.records.reduce(function (acc, feature) {
+                    acc.push(
+                        {
+                            type:"Feature",
+                            id: feature.fields.id,
+                            geometry: feature.fields.geo_shape,
+                            properties:{
+                                pos_sol: feature.fields.pos_sol,
+                                franchisst:feature.fields.franchisst,
+                                fictif:feature.fields.fictif,
+                                regime:feature.fields.regime,
+                                nom:feature.fields.nom
+                            }
+                        }
+                    )
+                    return acc
+                }, []);
+            return {
+                type: 'FeatureCollection',
+                features: _.chain(acc)
+                    // .filter(function(f){
+                    //     return f.id.startsWith(idSection)
+                    // })
+                    .value()
+            }
+    })
+}
+
+
+function sortByLabel(features) {
+    return _.sortBy(features, function (f) { return f.properties.label })
 }
